3a14f2e74c78ab6ef6d76d2b46d15e1e3e61b328
honda.j <honda.jun@bpsinc.jp>
2019-03-25 20:46:54 +0900

    implement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use colored::*;
+use csv::Reader;
+use std::io::{BufWriter, Write};
+use Color::*;
+
+const COLORS: &[Color] = &[Blue, Magenta, Cyan, Yellow, Green, Red];
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let mut reader = Reader::from_reader(std::io::stdin());
+    let out = std::io::stdout();
+    let mut writer = BufWriter::new(out.lock());
+
+    for record in reader.records() {
+        let mut colors = COLORS.iter().cycle();
+        for item in record?.iter() {
+            let color = colors.next().ok_or("Color is none.")?;
+            let item = item.color(*color);
+            writer.write(format!("{} ", item).as_bytes())?;
+        }
+        writer.write(b"\n")?;
+    }
+    Ok(())
 }
